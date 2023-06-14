0138c293032bcf5f9dc0595729d56df3ea20fdc2
tmrke <namat9@yandex.ru>
2023-06-14 12:44:38 +0700

    Update

---
+++ src/main/java/ru/ageev/springcourse/controllers/FirstController.java
@@ -0,0 +1,25 @@
+package ru.ageev.springcourse.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+}
